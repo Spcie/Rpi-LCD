67f8cfb4f939c2f8e7bb29c0007b9801cfa77dba
Spcie <m15188159686@163.com>
2017-10-25 19:19:36 +0800

    Create soft_spi.h

---
+++ sim/soft_spi.h
@@ -0,0 +1,13 @@
+#ifndef __SOFT_SPI_H
+#define __SOFT_SPI_H
+
+void soft_spi_init(volatile unsigned int*  peripherals_base);
+void soft_spi_setCE(unsigned char ce);
+void soft_spi_setDC(unsigned char dc);
+void soft_spi_rest(unsigned char rest);
+void soft_spi_wirteBus(unsigned char byte);
+void soft_spi_delayMs(unsigned int ms);
+void soft_spi_delayUs(unsigned int us);
+void soft_spi_uninit(void);
+
+#endif
